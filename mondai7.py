76b74105925afaa86ed6fc177b24e1e47d20fdf0
moyac1012 <moyac@realidea.jp>
2025-02-20 19:34:09 +0900

    mondai7

---
+++ mondai7.py
@@ -0,0 +1,18 @@
+n = int(input("行数を入力してください："))
+
+# forの場合
+for i in range(1, n+1):
+    print("*"*i)
+
+# whileの場合
+i = 1
+while i <= n:
+    print("*"*i)
+    i += 1
+
+
+# 別解
+for i in range(1, n+1):
+    for j in range(i):
+        print("*", end="")
+    print()
