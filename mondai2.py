7076398ff9e2a9ad8efffcad15745a203e0c13a2
moyac1012 <moyac@realidea.jp>
2025-02-20 19:13:09 +0900

    whileのパターンを追加

--- mondai2.py
+++ mondai2.py
@@ -1,5 +1,11 @@
 start_num = int(input("開始の数値："))
 end_num = int(input("修了の数値："))
 
+# forを使うパターン
 for i in range(start_num, end_num+1):
     print(i)
+
+# whileを使うパターン
+while start_num <= end_num:
+    print(start_num)
+    start_num += 1
