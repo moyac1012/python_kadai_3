17067d3220bb8018e6a3bdacc96942d43d433f1b
moyac1012 <moyac@realidea.jp>
2025-02-20 19:36:19 +0900

    修正

--- mondai2.py
+++ mondai2.py
@@ -1,11 +1,11 @@
 start_num = int(input("開始の数値："))
 end_num = int(input("修了の数値："))
 
-# forを使うパターン
-for i in range(start_num, end_num+1):
-    print(i)
-
-# whileを使うパターン
-while start_num <= end_num:
-    print(start_num)
-    start_num += 1
+if start_num <= end_num:
+    while start_num <= end_num:
+        print(start_num)
+        start_num += 1
+else:
+    while start_num >= end_num:
+        print(start_num)
+        start_num -= 1
